4ff5367ee4ab48a9636dc2156ecc4dbc26d57842
RomuloKennedy <82661711+RomuloKennedy@users.noreply.github.com>
2021-09-28 11:19:40 -0300

    Delete MeusProjetos_Python directory

--- MeusProjetos_Python/5_1_Wazeyes/Converter_geolocalizacao.py
+++
@@ -1,6 +0,0 @@
-from geopy.geocoders import Nominatim
-
-geolocator = Nominatim(user_agent="wazeyes") #Nome do Seu Aplicativo
-location = geolocator.geocode("175 5th Avenue NYC")
-print(location.address)
-print((location.latitude,location.longitude))
